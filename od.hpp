58ca80cc4bac6bc50331de4697434d8576145f3e
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2024-07-24 19:13:12 -0700

    move image stuff to separate file

--- od.hpp
+++ od.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include "yolov3.hpp"
 #include "yolov8.hpp"
+#include "img.hpp"
 
-#include <cassert>
 #include <vector>
 #include <string>
 
@@ -13,32 +13,7 @@ struct detection
     float x, y, w, h;
 };
 
-struct yolo_image {
-    int w, h, c;
-    std::vector<float> data;
 
-    yolo_image() : w(0), h(0), c(0) {}
-    yolo_image(int w, int h, int c) : w(w), h(h), c(c), data(w*h*c) {}
-
-    float get_pixel(int x, int y, int c) const {
-        assert(x >= 0 && x < w && y >= 0 && y < h && c >= 0 && c < this->c);
-        return data[c*w*h + y*w + x];
-    }
-
-    void set_pixel(int x, int y, int c, float val) {
-        assert(x >= 0 && x < w && y >= 0 && y < h && c >= 0 && c < this->c);
-        data[c*w*h + y*w + x] = val;
-    }
-
-    void add_pixel(int x, int y, int c, float val) {
-        assert(x >= 0 && x < w && y >= 0 && y < h && c >= 0 && c < this->c);
-        data[c*w*h + y*w + x] += val;
-    }
-
-    void fill(float val) {
-        std::fill(data.begin(), data.end(), val);
-    }
-};
 
 class object_detector {
 public:
